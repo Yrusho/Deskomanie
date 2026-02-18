d661312420b105aef7ebd14465e3ae9638f34f30
Todovka <jakub.todovic@gmail.com>
2026-02-18 16:15:12 +0100

    dskjaf

--- script.js
+++ script.js
@@ -177,3 +177,63 @@ document.getElementById("duch-kosik").addEventListener("click", function () {
         produkt.style.display = "none";
     }, 300);
 });
+
+function activity() {
+    document.getElementById("activity-karta").style.display = "block";
+}
+
+function dostihyasazky() {
+    document.getElementById("dostihyasazky-karta").style.display = "block";
+}
+
+function scrabble() {
+    document.getElementById("scrabble-karta").style.display = "block";
+}
+
+function monopoly() {
+    document.getElementById("monopoly-karta").style.display = "block";
+}
+
+function dixit() {
+    document.getElementById("dixit-karta").style.display = "block";
+}
+
+function carcassonne() {
+    document.getElementById("carcassonne-karta").style.display = "block";
+}
+
+function bang() {
+    document.getElementById("bang-karta").style.display = "block";
+}
+
+function krycijmena() {
+    document.getElementById("krycijmena-karta").style.display = "block";
+}
+
+function karak() {
+    document.getElementById("karak-karta").style.display = "block";
+}
+
+function vybusnekocky() {
+    document.getElementById("vybusnekocky-karta").style.display = "block";
+}
+
+function ubongo() {
+    document.getElementById("ubongo-karta").style.display = "block";
+}
+
+function tickettoride() {
+    document.getElementById("tickettoride-karta").style.display = "block";
+}
+
+function sedmdivusveta() {
+    document.getElementById("divu-sveta-karta").style.display = "block";
+}
+
+function jizdenkyprosim() {
+    document.getElementById("jizdenky-prosim-karta").style.display = "block";
+}
+
+function duch() {
+    document.getElementById("duch-karta").style.display = "block";
+}
